e8a1729289f5edcf08258853000be32dc8057a19
Aman <aman.deep@zooom-it.com>
2016-02-26 19:08:11 +0530

    barcode and qr code scanner implemented.

--- BeaconsApp/app/src/main/java/com/beacons/app/beaconsapp/CodeEntryAvtivity.java
+++ BeaconsApp/app/src/main/java/com/beacons/app/beaconsapp/CodeEntryAvtivity.java
@@ -3,12 +3,6 @@ package com.beacons.app.beaconsapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.RelativeLayout;
-
-import com.beacons.app.slidingmenu.SlidingMenuSetup;
-import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
 
 public class CodeEntryAvtivity extends BaseActivity {
@@ -29,6 +23,13 @@ public class CodeEntryAvtivity extends BaseActivity {
             public void onClick(View v) {
                 startActivity(new Intent(CodeEntryAvtivity.this,MyEventsActivity.class));
                 CodeEntryAvtivity.this.finish();
+            }
+        });
+
+        findViewById(R.id.scanner_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CodeEntryAvtivity.this,ScannerActivity.class));
             }
         });
     }
